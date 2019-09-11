e6c91aa29ee78f3ad818495dcfa54350ea962fa0
vitorregisr <vitorregisrr@gmail.com>
2019-09-11 15:50:13 -0300

    Improve main-nav dropdown to working responsive

--- js/responsivenav.js
+++ js/responsivenav.js
@@ -4,4 +4,15 @@
     $('.nav-toggler').click(function () {
         $('#main-nav-menu').toggleClass('active');
     });
+
+    $('.main-nav .has-dropdown > a').click(function (e) {
+        e.preventDefault();
+        
+        $('.main-nav__dropdown.active').removeClass('active');
+        $(this)
+            .parent()
+            .find('.main-nav__dropdown')
+            .toggleClass('active');
+    });
+
 })();
