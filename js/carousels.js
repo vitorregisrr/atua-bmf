34fae68d4e6120e9a321af26e24e23447373bd6e
vitorregisr <vitorregisrr@gmail.com>
2019-09-11 14:32:22 -0300

    Improve footer styling, Improve last home sections

--- js/carousels.js
+++ js/carousels.js
@@ -31,5 +31,42 @@
         }, 2)
     });
 
+    // Clientes Carousel //
+    const clientesCarousel = $(".owl-carousel.nossos-clientes__carousel");
+    clientesCarousel.owlCarousel({
+        autoplay: true,
+        autoPlaySpeed: 5000,
+        autoPlayTimeout: 5000,
+        autoplayHoverPause: true,
+        loop: true,
+        margin: 30,
+        center: false,
+        dots: false,
+        nav: true,
+        navText: [
+            "<i class='owl-nav-btn fa fa-chevron-circle-left mr-2'>", "<i class='owl-nav-btn fa fa-chevron-circle-right'>"
+        ],
+        slideBy: "page",
+        startPosition: 0,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        responsive: {
+            0: {
+                items: 1,
+                margin: 10
+            },
+
+            768: {
+                items: 4,
+                margin: 15
+            },
+
+            992: {
+                items: 5,
+                margin: 15
+            }
+        }
+    });
+
     window.dispatchEvent(new Event('resize'));
 })();
