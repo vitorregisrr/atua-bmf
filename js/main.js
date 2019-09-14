4a1888f50a04ef5effcf90e29c8c02bbfdecc752
vitorregisr <vitorregisrr@gmail.com>
2019-09-14 15:48:32 -0300

    Add contact-form validation, Misc styling and animations improvements

--- js/main.js
+++ js/main.js
@@ -1,28 +1,6 @@
-(function () {
-    'use strict';
-
-    //Init Libs
-    new WOW().init();
-    $('.lazy').lazyload();
-
-    //scroll top
-    var scrollTopBtn = $('.scroll-top');
-
-    $(window).scroll(function () {
-        if ($(window).scrollTop() > 300) {
-            scrollTopBtn.addClass('show');
-        } else {
-            scrollTopBtn.removeClass('show');
-        }
-    });
-
-    scrollTopBtn.on('click', function (e) {
-        e.preventDefault();
-        $('html, body').animate({
-            scrollTop: 0
-        }, '300');
-    });
-
-    window.dispatchEvent(new Event('resize'));
-
-})();
+// Use apenas em um enviroment com webpack!
+// require('./misc');
+// require('./carousels');
+// require('./inputmasks');
+// require('./form-validation');
+// require('./responsivenav');
